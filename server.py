191773dfdd12ea6aeabc4661e8c2207cd1ffa994
Oliver Bünting <oliver@brainbot.com>
2018-11-09 13:44:43 +0100

    Added error logs

--- server.py
+++ server.py
@@ -21,41 +21,47 @@ class Server:
     def barrier_triggered(self):
         try:
             self.conn.send("t")
-        except BrokenPipeError:
+        except BrokenPipeError as e:
+            log.error("Connection to Socket died with error %s" % e)
             self.conn, self.addr = self.socket.accept()
             self.barrier_triggered()
 
     def new_receiver(self, address_id):
         try:
             self.conn.send(address_id)
-        except BrokenPipeError:
+        except BrokenPipeError as e:
+            log.error("Connection to Socket died with error %s" % e)
             self.conn, self.addr = self.socket.accept()
             self.new_receiver(address_id)
 
     def start(self):
         try:
             self.conn.send("s")
-        except BrokenPipeError:
+        except BrokenPipeError as e:
+            log.error("Connection to Socket died with error %s" % e)
             self.conn, self.addr = self.socket.accept()
             self.start()
 
     def payment_received(self):
         try:
             self.conn.send("p")
-        except BrokenPipeError:
+        except BrokenPipeError as e:
+            log.error("Connection to Socket died with error %s" % e)
             self.conn, self.addr = self.socket.accept()
             self.payment_received()
 
     def payment_missing(self):
         try:
             self.conn.send("m")
-        except BrokenPipeError:
+        except BrokenPipeError as e:
+            log.error("Connection to Socket died with error %s" % e)
             self.conn, self.addr = self.socket.accept()
             self.payment_missing()
 
     def end(self):
         try:
             self.conn.close()
-        except BrokenPipeError:
+        except BrokenPipeError as e:
+            log.error("Connection to Socket died with error %s" % e)
             self.conn, self.addr = self.socket.accept()
             self.end()
